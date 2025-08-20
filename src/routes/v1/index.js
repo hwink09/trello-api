b064503497c8703e41acb09435dadaf5bb826703
hwink09 <caotranhoangminh2003@gmail.com>
2025-08-21 02:25:23 +0700

    Normalize line endings to CRLF for Windows

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,1 +1,15 @@
-// v1 routes
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+import { boardRoutes } from "~/routes/v1/boardRoutes";
+
+const Router = express.Router();
+
+/** Check APIs V1 status */
+Router.get("/status", (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "APIs V1 are ready to use." });
+});
+
+/** Board APIs */
+Router.use("/boards", boardRoutes);
+
+export const APIs_V1 = Router;
