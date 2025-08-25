dbc6529e51eb73c8b9f80bc6a46374810bc06583
hwink09 <caotranhoangminh2003@gmail.com>
2025-08-26 05:29:06 +0700

    Add error handling middleware and custom ApiError class

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import exitHook from "async-exit-hook";
 import { CONNECT_DB, CLOSE_DB } from "~/config/mongodb";
 import { env } from "~/config/environment";
 import { APIs_V1 } from "~/routes/v1";
+import { errorHandlingMiddleware } from "~/middlewares/errorHandlingMiddleware";
 
 const START_SERVER = async () => {
   const app = express();
@@ -13,6 +14,9 @@ const START_SERVER = async () => {
 
   // use API V1
   app.use("/v1", APIs_V1);
+
+  // Middleware xử lí lỗi tập trung
+  app.use(errorHandlingMiddleware);
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(
