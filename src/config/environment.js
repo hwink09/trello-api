28bc8fa64bd1ad5568066f8a8a72437c65486999
hwink09 <caotranhoangminh2003@gmail.com>
2025-09-22 13:53:37 +0700

    Update

--- src/config/environment.js
+++ src/config/environment.js
@@ -4,10 +4,11 @@ export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
 
-  APP_HOST: process.env.APP_HOST,
-  APP_PORT: process.env.APP_PORT,
+  // Render sẽ cung cấp process.env.PORT, nếu không có thì dùng APP_PORT trong .env hoặc mặc định 8017
+  APP_HOST: process.env.APP_HOST || "0.0.0.0",
+  APP_PORT: process.env.PORT || process.env.APP_PORT || 8017,
 
   BUILD_MODE: process.env.BUILD_MODE,
 
-  AUTHOR: process.env.AUTHOR,
+  AUTHOR: process.env.AUTHOR || "Anonymous",
 };
