05828beca99367c4a5d4bf6e573c413dbe7c9154
hwink09 <caotranhoangminh2003@gmail.com>
2025-09-26 12:44:01 +0700

    Implement user registration feature with validation and user mode

---
+++ src/models/userModel.js
@@ -0,0 +1,108 @@
+import Joi from 'joi'
+import { get } from 'lodash'
+import { ObjectId, ReturnDocument } from 'mongodb'
+import { GET_DB } from '~/config/mongodb'
+import { EMAIL_RULE, EMAIL_RULE_MESSAGE } from '~/utils/validators'
+
+// Define tạm 2 roles cho user
+const USER_ROLES = {
+  CLIENT: 'client',
+  ADMIN: 'admin'
+}
+
+// Define Collection (name & schema)
+const USER_COLLECTION_NAME = 'users'
+const USER_COLLECTION_SCHEMA = Joi.object({
+  email: Joi.string()
+    .required()
+    .pattern(EMAIL_RULE)
+    .message(EMAIL_RULE_MESSAGE),
+  password: Joi.string().required(),
+  // username cắt ra từ email sẽ có khả năng không unique bởi vì sẽ có những tên email trùng nhau nhưng từ những nhà cung cấp khác nhau
+  username: Joi.string().required().trim().strict(),
+  displayName: Joi.string().required().trim().strict(),
+  avatar: Joi.string().default(null),
+  role: Joi.string()
+    .valid(USER_ROLES.CLIENT, USER_ROLES.ADMIN)
+    .default(USER_ROLES.CLIENT),
+
+  isActive: Joi.boolean().default(false),
+  verifyToken: Joi.string(),
+
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+// Chỉ định ra những Fields mà chúng ta không muốn cho phép cập nhật trong hàm update()
+const INVALID_UPDATE_FIELDS = ['_id', 'email', 'username', 'createdAt']
+
+const validateBeforeCreate = async (data) => {
+  return await USER_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data)
+    const createdUser = await GET_DB()
+      .collection(USER_COLLECTION_NAME)
+      .insertOne(validData)
+    return createdUser
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOneById = async (userId) => {
+  try {
+    const result = await GET_DB()
+      .collection(USER_COLLECTION_NAME)
+      .findOne({ _id: new ObjectId(userId) })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOneByEmail = async (emailValue) => {
+  try {
+    const result = await GET_DB()
+      .collection(USER_COLLECTION_NAME)
+      .findOne({ email: emailValue })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const update = async (userId, updateData) => {
+  try {
+    // Lọc từng field mà mình không cho phép cập nhật linh tinh
+    Object.keys(updateData).forEach((fieldName) => {
+      if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+        delete updateData[fieldName]
+      }
+    })
+
+    const result = await GET_DB()
+      .collection(USER_COLLECTION_NAME)
+      .findOneAndUpdate(
+        { _id: new ObjectId(userId) },
+        { $set: updateData },
+        { returnDocument: 'after' } // trả về kết quả mới sau khi update
+      )
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const userModel = {
+  USER_COLLECTION_NAME,
+  USER_COLLECTION_SCHEMA,
+  USER_ROLES,
+  createNew,
+  findOneById,
+  findOneByEmail,
+  update
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,24 +1,28 @@
-import express from "express";
-import { StatusCodes } from "http-status-codes";
-import { boardRoute } from "~/routes/v1/boardRoute";
-import { columnRoute } from "~/routes/v1/columnRoute";
-import { cardRoute } from "~/routes/v1/cardRoute";
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoute } from '~/routes/v1/boardRoute'
+import { columnRoute } from '~/routes/v1/columnRoute'
+import { cardRoute } from '~/routes/v1/cardRoute'
+import { userRoute } from '~/routes/v1/userRoute'
 
-
-const Router = express.Router();
+const Router = express.Router()
 
 /** Check APIs V1 status */
-Router.get("/status", (req, res) => {
-  res.status(StatusCodes.OK).json({ message: "APIs V1 are ready to use." });
-});
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.' })
+})
 
 /** Board APIs */
-Router.use("/boards", boardRoute);
+Router.use('/boards', boardRoute)
 
 /** Column APIs */
-Router.use("/columns", columnRoute);
+Router.use('/columns', columnRoute)
 
 /** Card APIs */
-Router.use("/cards", cardRoute);
+Router.use('/cards', cardRoute)
 
-export const APIs_V1 = Router;
+/** User APIs */
+Router.use('/users', userRoute)
+
+
+export const APIs_V1 = Router
