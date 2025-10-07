2998db044967237fcd5b777ae9cba14a9e76160b
hwink09 <caotranhoangminh2003@gmail.com>
2025-10-07 11:32:28 +0700

    Refactor board-related services and controllers; add pagination defaults and enhance board retrieval logic

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,55 +1,69 @@
-import { StatusCodes } from "http-status-codes";
-import { boardService } from "~/services/boardService";
+import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
     // Điều hướng dữ liệu sang Service
-    const createdBoard = await boardService.createNew(req.body);
+    const createdBoard = await boardService.createNew(req.body)
 
     // throw new ApiError(StatusCodes.BAD_REQUEST, "Hwink test error");
 
     // Có kết quả thì trả về Client
-    res.status(StatusCodes.CREATED).json(createdBoard);
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
+
+const getBoards = async (req, res, next) => {
+  try {
+    const userId = req.jwtDecoded._id
+    // page và itemPerPage sẽ được truyền vào từ query url phía FE nên BE lấy thông qua req.query
+    const { page, itemsPerPage } = req.query
+    const result = await boardService.getBoards(userId, page, itemsPerPage)
+
+    res.status(StatusCodes.OK).json(result)
+  } catch (error) {
+    next(error)
+  }
+}
 
 const getDetails = async (req, res, next) => {
   try {
-    const boardId = req.params.id;
+    const boardId = req.params.id
     // sau này sẽ có thêm userId sẽ chỉ lấy board thuộc về user đó
-    const board = await boardService.getDetails(boardId);
+    const board = await boardService.getDetails(boardId)
 
-    res.status(StatusCodes.OK).json(board);
+    res.status(StatusCodes.OK).json(board)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 const update = async (req, res, next) => {
   try {
-    const boardId = req.params.id;
-    const updatedBoard = await boardService.update(boardId, req.body);
+    const boardId = req.params.id
+    const updatedBoard = await boardService.update(boardId, req.body)
 
-    res.status(StatusCodes.OK).json(updatedBoard);
+    res.status(StatusCodes.OK).json(updatedBoard)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 const moveCardToDifferentColumn = async (req, res, next) => {
   try {
-    const result = await boardService.moveCardToDifferentColumn(req.body);
-    res.status(StatusCodes.OK).json(result);
+    const result = await boardService.moveCardToDifferentColumn(req.body)
+    res.status(StatusCodes.OK).json(result)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 export const boardController = {
   createNew,
+  getBoards,
   getDetails,
   update,
-  moveCardToDifferentColumn,
-};
+  moveCardToDifferentColumn
+}
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,103 +1,121 @@
-import { slugify } from "~/utils/formatters";
-import { boardModel } from "~/models/boardModel";
-import ApiError from "~/utils/ApiError";
-import { StatusCodes } from "http-status-codes";
-import { cloneDeep } from "lodash";
+import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
+import { cloneDeep } from 'lodash'
 
-import { columnModel } from "~/models/columnModel";
-import { cardModel } from "~/models/cardModel";
+import { columnModel } from '~/models/columnModel'
+import { cardModel } from '~/models/cardModel'
+import { DEFAULT_ITEMS_PER_PAGE, DEFAULT_PAGE } from '~/utils/constants'
 
 /* eslint-disable no-useless-catch */
 const createNew = async (reqBody) => {
   try {
     const newBoard = {
       ...reqBody,
-      slug: slugify(reqBody.title),
-    };
+      slug: slugify(reqBody.title)
+    }
 
     // Gọi tới Model để xử lí lưu bản ghi newBoard vào trong DB
-    const createdBoard = await boardModel.createNew(newBoard);
+    const createdBoard = await boardModel.createNew(newBoard)
 
     // Lấy bản ghi board sau khi gọi
-    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId);
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
 
     // Làm thêm các xử lí logic khác với các Collection khác tùy đặc thù dự án...
     // Bắn email, notification về cho admin khi có 1 board mới được tạo...
 
     // Trả kết quả về, trong service luôn phải có return
-    return getNewBoard;
+    return getNewBoard
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
+
+const getBoards = async (userId, page, itemsPerPage) => {
+  try {
+    if (!page) page = DEFAULT_PAGE
+    if (!itemsPerPage) itemsPerPage = DEFAULT_ITEMS_PER_PAGE
+
+    const result = await boardModel.getBoards(
+      userId,
+      parseInt(page, 10),
+      parseInt(itemsPerPage, 10)
+    )
+    return result
+  } catch (error) {
+    throw error
+  }
+}
 
 const getDetails = async (boardId) => {
   try {
-    const board = await boardModel.getDetails(boardId);
+    const board = await boardModel.getDetails(boardId)
     if (!board) {
-      throw new ApiError(StatusCodes.NOT_FOUND, "Board not found");
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
     }
 
     // 1. DeepClone board ra một cái mới để xử lí, không ảnh hưởng tới board ban đầu, tùy mục đích có cần hay không
-    const resBoard = cloneDeep(board);
+    const resBoard = cloneDeep(board)
     // 2. Đưa card về đúng column của nó
     resBoard.columns.forEach((column) => {
       column.cards = resBoard.cards.filter((card) =>
         card.columnId.equals(column._id)
-      );
+      )
       // column.cards = resBoard.cards.filter(
       //   (card) => card.columnId.toString() === column._id.toString()
       // );
-    });
+    })
     //xóa mảng cards khỏi board ban đầu
-    delete resBoard.cards;
+    delete resBoard.cards
 
-    return resBoard;
+    return resBoard
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const update = async (boardId, reqBody) => {
   try {
     const updateData = {
       ...reqBody,
-      updatedAt: Date.now(),
-    };
-    const updatedBoard = await boardModel.update(boardId, updateData);
+      updatedAt: Date.now()
+    }
+    const updatedBoard = await boardModel.update(boardId, updateData)
 
-    return updatedBoard;
+    return updatedBoard
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 const moveCardToDifferentColumn = async (reqBody) => {
   try {
     //B1: cập nhật mảng cardOrderIds của Column ban đầu chứa nó (xóa _id của Card ra khỏi mảng)
     await columnModel.update(reqBody.prevColumnId, {
       cardOrderIds: reqBody.prevCardOrderIds,
-      updatedAt: Date.now(),
-    });
+      updatedAt: Date.now()
+    })
     // B2: Cập nhật mảnh CardOrderIds của Column tiếp theo (thêm _id của Card vào mảng)
     await columnModel.update(reqBody.nextColumnId, {
       cardOrderIds: reqBody.nextCardOrderIds,
-      updatedAt: Date.now(),
-    });
+      updatedAt: Date.now()
+    })
     // B3: Cập nhật lại trường columnId mới của cái card đã kéo
     await cardModel.update(reqBody.currentCardId, {
       columnId: reqBody.nextColumnId
-    });
+    })
 
-    return { updateResult: "Successfully" };
+    return { updateResult: 'Successfully' }
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 export const boardService = {
   createNew,
+  getBoards,
   getDetails,
   update,
-  moveCardToDifferentColumn,
-};
+  moveCardToDifferentColumn
+}
